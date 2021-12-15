283526d7dac187f3e566056256b2c5263db4c15b
Wellington Lopes <wellyofgod@gmail.com>
2021-12-15 14:58:02 -0300

    Feat: Parameterizing route resource student

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\StudentController;
+
 
 Route::get('login', [LoginController::class, 'login'])->name('login');
 Route::post('auth', [LoginController::class, 'auth'])->name('auth');
 Route::get('home', [LoginController::class, 'home'])->middleware('auth')->name('home');
 Route::get('logout', [LoginController::class, 'logout'])->middleware('auth')->name('logout');
 Route::get('admin', [LoginController::class, 'admin'])->middleware(['auth', 'isAdmin'])->name('admin');
+
+Route::resource('student', StudentController::class);
+
 
 
