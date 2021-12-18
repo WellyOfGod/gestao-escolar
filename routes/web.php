b018599b05b40559a6ce471f2ff5b0557182aeee
Wellington Lopes <wellyofgod@gmail.com>
2021-12-18 12:49:14 -0300

    Refactor: Parameterizing group of authenticated and unauthenticated routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\{LoginController, StudentController, CourseController, DisciplineController};
+
 
 
 Route::get('login', [LoginController::class, 'login'])->name('login');
 Route::post('auth', [LoginController::class, 'auth'])->name('auth');
-Route::get('home', [LoginController::class, 'home'])->middleware('auth')->name('home');
-Route::get('logout', [LoginController::class, 'logout'])->middleware('auth')->name('logout');
-Route::get('admin', [LoginController::class, 'admin'])->middleware(['auth', 'isAdmin'])->name('admin');
 
-Route::resource('student', StudentController::class);
+Route::group(['middleware' => 'auth'], function(){
+    //Routes Users
+    Route::get('home', [LoginController::class, 'home'])->name('home');
+    Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+    Route::resource('student', StudentController::class);
+    Route::resource('course', CourseController::class);
+    Route::resource('discipline', DisciplineController::class);
+
+    //Routes Admin
+    Route::group(['middleware' => 'isAdmin'], function(){
+        Route::get('admin', [LoginController::class, 'admin'])->name('admin');
+    });
+});
+
+
 
 
 
