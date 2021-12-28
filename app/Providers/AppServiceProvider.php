5f585a7fe71abec8f5a44b3aab9764123c4fd60d
Wellington Lopes <wellyofgod@gmail.com>
2021-12-28 15:10:01 -0300

    Feat:Added Paginator::useBootstrap() method, to perform the view page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         Schema::defaultStringLength(191);
     }
@@ -23,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        Paginator::useBootstrap();
     }
 }
