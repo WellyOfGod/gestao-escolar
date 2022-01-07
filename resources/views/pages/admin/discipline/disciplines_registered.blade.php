23eca4320c9141f179b6c2975ab9dee54124c6a9
Wellington Lopes <wellyofgod@gmail.com>
2022-01-07 17:47:25 -0300

    Refactor: Adjusting table view

--- resources/views/pages/admin/discipline/disciplines_registered.blade.php
+++ resources/views/pages/admin/discipline/disciplines_registered.blade.php
@@ -16,22 +16,42 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
+                    <div class="card-header">
+                        <a class="btn btn-primary text-white" href="{{ route('discipline.create') }}">
+                            Cadastrar Disciplina
+                        </a>
+                    </div>
                     <div class="card-body">
-                        <div class="section-title mt-0">Light</div>
                         <table class="table table-borderless">
                             <thead>
-                            <tr>
+                            <tr class="text-center">
                                 <th scope="col">Disciplina</th>
                                 <th scope="col">Curso</th>
                                 <th scope="col">Ações</th>
                             </tr>
                             </thead>
-                            <tbody>
+                            <tbody class="text-center">
                             @foreach($disciplines as $discipline)
+                                <form id="form-destroy"
+                                      action="{{ route('discipline.destroy', $discipline->id) }}"
+                                      method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                </form>
                                 <tr>
                                     <td>{{ $discipline->name }}</td>
                                     <td>{{ $discipline->course->name }}</td>
-                                    <td>@mdo</td>
+                                    <td>
+                                        <div class="buttons">
+                                            <a href="{{ route('discipline.edit', $discipline->id) }}"
+                                               class="btn btn-icon btn-primary">
+                                                <i class="far fa-edit"></i>
+                                            </a>
+                                            <button form="form-destroy" class="btn btn-icon btn-danger">
+                                                <i class="fas fa-times"></i>
+                                            </button>
+                                        </div>
+                                    </td>
                                 </tr>
                             @endforeach
                             </tbody>
