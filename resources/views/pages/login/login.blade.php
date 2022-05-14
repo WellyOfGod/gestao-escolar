90280715de8d338c18625fe414fc6b6cbd268b0a
Wellington Lopes <wellyofgod@gmail.com>
2022-05-14 00:08:36 -0300

    x

--- resources/views/pages/login/login.blade.php
+++ resources/views/pages/login/login.blade.php
@@ -10,7 +10,7 @@
                     </div>
 
                     <div class="card card-primary">
-                        <div class="card-header"><h4>Login</h4></div>
+                        <div class="card-header"><h4>Login2</h4></div>
                         <div class="card-body">
                             <form method="POST" action="{{ route('auth') }}" class="needs-validation" novalidate="">
                                 @csrf
